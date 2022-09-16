c14969aafa813337043048d71d2e292ddfd13d8e
pushpendrasingh <pushpendra.singh@likeminds.community>
2022-09-16 20:45:03 +0530

    sdk v1.1

---
+++ LikeMindsChat.xcframework/ios-arm64/LikeMindsChat.framework/Headers/LikeMindsChat.h
@@ -0,0 +1,18 @@
+//
+//  LikeMindsChat.h
+//  LikeMindsChat
+//
+//  Created by Pushpendra Singh on 29/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LikeMindsChat.
+FOUNDATION_EXPORT double LikeMindsChatVersionNumber;
+
+//! Project version string for LikeMindsChat.
+FOUNDATION_EXPORT const unsigned char LikeMindsChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LikeMindsChat/PublicHeader.h>
+
+
